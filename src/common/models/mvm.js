e5c05fe5ca7ce51936107e1a9e25447000bfc5b9
JiriHusak-lab <56121194+JiriHusak-lab@users.noreply.github.com>
2020-11-11 20:21:19 +0100

    Update mvm.js

--- src/common/models/mvm.js
+++ src/common/models/mvm.js
@@ -3,7 +3,10 @@
 module.exports = function(Mvm) {
     Mvm.listAll = function(cb) {
         console.log('mvms/listAll invoked');
-        Mvm.find({}, cb);
+        // Fake JHUSAK 2020.11.11
+        cb = {'mvm': 'wh1', 'mvm': 'wh2', 'mvm': 'wh3','mvm': 'mmswh18'};
+        // Orig - stopped JHUSAK 2020.11.11
+        // Mvm.find({}, cb);
         console.log('mvms/listAll passed.');
     };
 
